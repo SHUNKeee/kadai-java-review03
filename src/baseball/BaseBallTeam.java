874190525b151240c872c1e5bd5c4c270ca13a10
SHUNKeee <kamishun15@gmail.com>
2024-08-16 19:58:14 +0900

    カプセル化追加、アクセス修飾子追加

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -1,22 +1,30 @@
 package baseball;
 
 public class BaseBallTeam {
-	//フィールド
-	protected String name;
-	protected int win ;
-	protected int lose;
-	protected int draw;
+    //フィールド
+    private String name;
+    private int win ;
+    private int lose;
+    private int draw;
+
+public BaseBallTeam(String name,int win,int lose,int draw) {
+    this.name = name;
+    this.win = win;
+    this.lose = lose;
+    this.draw = draw;
+
+}
 
 //勝率を出すメソッド
- double getRate() {
-	double rate = (double)win/(double)(win + lose);
-		return rate;
+ private double getRate() {
+    double rate = (double)win/(double)(win + lose);
+        return rate;
 }
- 
+
  //チームの結果を表示するメソッド
- void report() {
-	 System.out.println(name+"の2022年の成績は "+win+"勝 "+lose+"敗 "+draw+"分、勝率は "+getRate()+"です。");
-		 
+ public void report() {
+     System.out.println(name+"の2022年の成績は "+win+"勝 "+lose+"敗 "+draw+"分、勝率は "+getRate()+"です。");
+
  }
 
 }
--- src/baseball/Review03.java
+++ src/baseball/Review03.java
@@ -2,53 +2,24 @@ package baseball;
 
 public class Review03 {
 
-	public static void main(String[] args) {
-		
-		BaseBallTeam bb1 = new BaseBallTeam();
-		BaseBallTeam bb2 = new BaseBallTeam();
-		BaseBallTeam bb3 = new BaseBallTeam();
-		BaseBallTeam bb4 = new BaseBallTeam();
-		BaseBallTeam bb5 = new BaseBallTeam();
-		BaseBallTeam bb6 = new BaseBallTeam();
-		
-		bb1.name = "東京ヤクルトスワローズ";
-		bb1.win = 80;
-		bb1.lose = 59;
-		bb1.draw = 4;
-		
-		bb2.name = "横浜DeNAベイスターズ";
-		bb2.win = 73;
-		bb2.lose = 68;
-		bb2.draw = 2;
-		
-		bb3.name = "阪神タイガース";
-		bb3.win = 68;
-		bb3.lose = 71;
-		bb3.draw = 4;
-		
-		bb4.name = "読売ジャイアンツ";
-		bb4.win = 68;
-		bb4.lose = 72;
-		bb4.draw = 3;
-		
-		bb5.name = "広島東洋カープ";
-		bb5.win = 66;
-		bb5.lose = 74;
-		bb5.draw = 3;
-		
-		bb6.name = "中日ドラゴンズ";
-		bb6.win = 66;
-		bb6.lose = 75;
-		bb6.draw = 2;
-		
-		bb1.report();
-		bb2.report();
-		bb3.report();
-		bb4.report();
-		bb5.report();
-		bb6.report();
-	
+    public static void main(String[] args) {
 
-	}
+        BaseBallTeam bb1 = new BaseBallTeam("東京ヤクルトスワローズ",80,59,4);
+        BaseBallTeam bb2 = new BaseBallTeam("横浜DeNAベイスターズ",73,68,2);
+        BaseBallTeam bb3 = new BaseBallTeam("阪神タイガース",68,71,4);
+        BaseBallTeam bb4 = new BaseBallTeam("読売ジャイアンツ",68,72,3);
+        BaseBallTeam bb5 = new BaseBallTeam("広島東洋カープ",66,74,3);
+        BaseBallTeam bb6 = new BaseBallTeam("中日ドラゴンズ",66,75,2);
+
+
+        bb1.report();
+        bb2.report();
+        bb3.report();
+        bb4.report();
+        bb5.report();
+        bb6.report();
+
+
+    }
 
 }
